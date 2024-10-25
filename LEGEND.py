10489281d04e152466054ee7d0f74f0f0b1d3fd7
rb2906937 <rb2906937@gmail.com>
2024-10-26 01:21:49 +0530

    Update LEGEND.py

--- LEGEND.py
+++ LEGEND.py
@@ -101,7 +101,7 @@ async def handle_action(update: Update, context: ContextTypes.DEFAULT_TYPE):
 # Main function to start the bot
 def main():
     # Your Telegram bot token
-    TOKEN = "7074559050:AAE8P3KU9NvpUgKFxJXgl9NOoBUHWZK_SLY"
+    TOKEN = "7403028364:AAEzgUYQkiSylyNs9rc0v9FyNs7QpUzmlac"
 
     # Create Application object with your bot's token
     application = Application.builder().token(TOKEN).build()
